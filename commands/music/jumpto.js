9c6fa49bf1fc38f4b89224f2d35c868b27d4fbb1
kevin octavian <kebimbangan12@gmail.com>
2020-07-02 07:05:37 +0700

    Update jumpto.js

--- commands/music/jumpto.js
+++ commands/music/jumpto.js
@@ -43,6 +43,6 @@ module.exports = {
 
         serverQueue.connection.dispatcher.end();
 
-        msg.channel.send(`Jump to song ${queue}`);
+        msg.channel.send(`Jump to song ${queue[0]}`);
     },
 };
